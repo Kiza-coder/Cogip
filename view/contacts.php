b687b0e5af215ccc76ba6e680dd32ce51f8470f9
Julien Goetghebuer <goetghebuer.julien@gmail.com>
2019-10-31 12:15:42 +0100

    add files

--- view/contacts.php
+++
@@ -1,7 +0,0 @@
-<h1>CONTACTS </h1>
-
-<?php
-    $contact = $req -> fetchAll(PDO::FETCH_ASSOC);
-    print_r($contact);
-
-?>
---
+++ view/contactsView.php
@@ -0,0 +1,27 @@
+<h1>CONTACTS </h1>
+
+<?php
+    $contact = $req -> fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Phone</th>
+        <th>Email</th>
+        <th>Company</th>
+    </tr>
+    <?php
+
+foreach($contact as $key){
+$url = $key['id'];
+$firstname = $key['first_name'];
+$lastname = $key['last_name'];
+$phone = $key['phone'];
+$email = $key['email'];
+echo <<<EOF
+<tr><td><a href="detailsContact.php/id=$url">$firstname $lastname</a></td><td>$phone</td><td>$email</td></tr>
+EOF;
+}
+?>
+</table>
