3dc94a1e0fa7b602f8634d4362f2cd57a7a0e3f5
Julien Goetghebuer <goetghebuer.julien@gmail.com>
2019-11-08 16:43:20 +0100

    try to fixed invoices details

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ $_SESSION['rights'] = 1;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=value.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Cogip Database Manager - Create Read Update Delete any Clients or Providers" />
+
+
     <title>Document</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
